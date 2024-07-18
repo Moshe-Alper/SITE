4abc61bd9a1fd9f6ee6a2e626d532d01947fb437
Moshik Alper <moshikalper@gmail.com>
2024-07-18 11:47:57 +0300

    Add modal

--- js/blogin.js
+++ js/blogin.js
@@ -3,3 +3,9 @@
 function onToggleMenu() {
     document.body.classList.toggle('menu-open')
 }
+
+function onReadDetails() {
+    const elDetails = document.querySelector('.post-details')
+    console.log(elDetails);
+    elDetails.showModal()
+}
